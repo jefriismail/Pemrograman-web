fa7523c8ff7b387b39b58b29c356cccd9b4ee178
jefriismail <jefriismail99@gmail.com>
2020-10-13 19:24:12 +0700

    Tugas 4

---
+++ Tugas_4_[18081010059]/academic.php
@@ -0,0 +1,64 @@
+<?php
+    $title = 'Jefri Abdurrozak Ismail | Academic';
+    $avatar = './asset/Academic.png';
+    $line = './asset/Line 1.png';
+    $ellipse1 = './asset/Ellipse 1.png';
+    $ellipse2 = './asset/Ellipse 1.png';
+    $ellipse3 = './asset/Ellipse 1.png';
+    $ellipse4 = './asset/Ellipse 1.png';
+    $ellipse5 = './asset/Ellipse 1.png';
+    $ellipse6 = './asset/Ellipse 1.png';
+    $tahunsd = '2012';
+    $tahunsmp = '2015';
+    $tahunsma = '2018';
+    $tahunkuliah = 'Now';
+    $sd = 'SDN Putat Jaya V/381 Surabaya';
+    $smp = 'SMPN 4 Surabaya';
+    $sma = 'SMAN 6 Surabaya';
+    $kuliah = 'UPN "Veteran" Jawa Timur';
+?>
+
+<html>
+    <head>
+        <title><?= $title ?></title>
+        <!--Connect CSS-->
+        <link rel="stylesheet" href="academic.css">
+    </head>
+    <body id="umum">
+        <!--Content frame-->
+        <div id="container"></div>
+        <div id="segment"></div>
+
+        <!--Menu-->
+        <svg id="baracademic"></svg>
+        <a id="home" href="./index.php">Home</a>
+        <a id="biodata" href="./biodata.php">Biodata</a>
+        <a id="academic" href="./academic.php">Academic</a>
+        <a id="skills" href="./skills.php">Skills</a>
+
+        <!--avatar-->
+        <img id="avataracademic" src="<?= $avatar ?>" alt="">
+
+        <!--Header-->
+        <p id="academic">Academic</p>
+
+        <!--Timeline pict-->
+        <img id="line" src="<?= $line ?>" alt="">
+        <img id="ellipse1" src="<?= $ellipse1 ?>" alt="">
+        <img id="ellipse2" src="<?= $ellipse2 ?>" alt="">
+        <img id="ellipse3" src="<?= $ellipse3 ?>" alt="">
+        <img id="ellipse4" src="<?= $ellipse4 ?>" alt="">
+        <img id="ellipse5" src="<?= $ellipse5 ?>" alt="">
+        <img id="ellipse6" src="<?= $ellipse6 ?>" alt="">
+
+        <!--Timeline data-->
+        <p id="tahunsd"><?= $tahunsd ?></p>
+        <p id="sd"><?= $sd ?></p>
+        <p id="tahunsmp"><?= $tahunsmp?></p>
+        <p id="smp"><?= $smp?></p>
+        <p id="tahunsma"><?= $tahunsma?></p>
+        <p id="sma"><?= $sma?></p>
+        <p id="tahunkuliah"><?= $tahunkuliah?></p>
+        <p id="kuliah"><?= $kuliah?></p>
+    <body>
+<html>
---
+++ Tugas_4_[18081010059]/index.php
@@ -0,0 +1,61 @@
+<?php
+    $title = 'Jefri Abdurrozak Ismail | Home';
+    $profpic = './asset/IMG_20200826_150539.jpg';
+    $category1 = 'IT Enthusiast';
+    $category2 = 'Photographer';
+    $name = 'Jefri Abdurrozak Ismail';
+    $intro = '     I was born on November 4th 1999 at Surabaya and i am 20 years old now. I live in Surabaya for these years. I do not have any brother and sister but i have great relationship to all of my cousins. To kill silent in my house, i always play music all day and night.';
+    $readmore = 'Read More ► ';
+    $whatsapp = './asset/Asset 2.png';
+    $facebook = './asset/Asset 3.png';
+    $instagram = './asset/Asset 4.png';
+    $telepon = '+62 8785-563-0276';
+    $idfacebook = '@jefriismail99';
+    $idinstagram = '@jefriismail';
+
+?>
+
+<html>
+    <head>
+        
+        <title><?= $title; ?></title>
+        <!--Connect CSS-->
+        <link rel="stylesheet" href="main.css">
+        
+    </head>
+    <body id="umum">
+        <!--Content frame-->
+        <div id="container"></div>
+        <div id="segment"></div>
+
+        <!--Profpic-->
+        <img id="profpic" src="<?= $profpic ?>" alt="">
+
+        <!--Menu-->
+        <svg id="barhome"></svg>
+        <a id="home" href="./index.php">Home</a>
+        <a id="biodata" href="./biodata.php">Biodata</a>
+        <a id="academic" href="./academic.php">Academic</a>
+        <a id="skills" href="./skills.php">Skills</a>
+
+        <!--Category-->
+        <svg id="category1"></svg>
+        <svg id="category2"></svg>
+        <p id="category1"><?= $category1 ?></p>
+        <p id="category2"><?= $category2 ?></p>
+
+        <!--Profile description-->
+        <p id="profdesc"><?= $name ?></p>
+        <p id="intro"><?= $intro ?></p>
+        <a id="readmore" href="./biodata.php"><?= $readmore ?></a>
+
+        <!--Find me-->
+        <p id="findme">Find me at :</p>
+        <img id="whatsapp" src="<?= $whatsapp ?>" alt="">
+        <p id="telepon"><?= $telepon ?></p>;
+        <img id="facebook" src="<?= $facebook ?>" alt="">
+        <p id="idfacebook"><?= $idfacebook?></p>
+        <img id="instagram" src="<?= $instagram?>" alt="">
+        <p id="idinstagram"><?= $idinstagram?></p>
+    </body>
+</html>
